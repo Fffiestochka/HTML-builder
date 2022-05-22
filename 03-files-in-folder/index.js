7f297195680d10541968d288242070f7ac17955f
Fffiestochka <Fffiestochka@mail.ru>
2022-05-22 21:57:00 +0300

    feat: task 3 done

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,31 @@
+// Импорт всех требуемых модулей
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs/promises');
+// Чтение содержимого папки secret-folder
+const pathToFolder = path.join(__dirname, 'secret-folder');
+let fileName;
+let fileType;
+let fileSize;
+fsPromises.readdir(pathToFolder, { withFileTypes: true }).then((files) => {
+  files.forEach((file) => {
+    if (file.isFile) {
+      let pathToFile = path.join(pathToFolder, file.name);
+      // console.log(fileType);
+      fs.stat(pathToFile, (err, stats) => {
+        if (err) throw err;
+        if (stats.isFile) {
+          // console.log(stats);
+          fileType = path.extname(file.name).slice(1);
+          fileSize = stats.size;
+          fileName = path.parse(pathToFile).name;
+          console.log(`${fileName} - ${fileType} - ${fileSize}`);
+        }
+      });
+    }
+  });
+});
+
+// Получение данных о каждом объекте который содержит папка secret-folder
+// Проверка объекта на то, что он является файлом
+// Вывод данных о файле в консоль
